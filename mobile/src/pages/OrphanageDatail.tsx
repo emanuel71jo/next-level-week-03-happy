72e807a2733486be0d070994d5821dc6369756b0
emanuel71jo <emanuel71jo@gmail.com>
2020-10-18 12:02:36 -0300

    Fourth day, nlw 03

---
+++ mobile/src/pages/OrphanageDatail.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+// import { Container } from './styles';
+
+const OrphanageDetail: React.FC = () => {
+  return <View />;
+};
+
+export default OrphanageDetail;
